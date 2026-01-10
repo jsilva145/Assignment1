e90140a10ac9b72b539269f4ddc47b62de411992
jsilva145 <jsilva145@student.rccd.edu>
2026-01-10 23:39:51 +0000

    No change.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!\n"
-              << "Jose Silva.\n"
-              << "I have goals in the gym, but my main goal is to have a job that allows creativity and pays well.\n"
-              << "My current interests are the gym, drawing, and gaming.\n"
-              << "My dream is to create a great story that inspires many.\n"
-              << "In one year I expect to be done at MVC. In three, half way done with university. In five, done with school. In seven, starting my career as a game developer.\n";
-    return 0;
-}
+int main() { 
+std::cout << "Hello World!\n";
+std::cout << "Jose Silva.\n";  
+std::cout << "I have goals in the gym, but my main goal is to have a job that allows" " creativity and pays well. \n"; 
+std::cout << "My current interests are the gym, drawing, and gaming. \n"; 
+std::cout << "My dream is to create a great story that inspires many. \n"; 
+std::cout << "In one year I expect to be done at MVC. \n";
+std::cout << "In three, half way done with university. \n"; 
+std::cout << "In five, done with school. \n";
+std::cout << "In seven, starting my career as a game developer.\n"; 
+  return 0;}
