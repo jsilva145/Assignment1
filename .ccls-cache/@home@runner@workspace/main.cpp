83939e351e2281eed0c6b50e3bd0cc330880e320
jsilva145 <52539884-jsilva145@users.noreply.replit.com>
2026-01-10 23:05:12 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() { std::cout << "Hello World! Jose Silva. I have goals in the gym, but my main goal is to have a job that allows creativity and pays well. My current interests are the gym, drawing, and gaming. 
+My dream is to create a great story that inspires many. 
+In one year I expect to be done at MVC. In three, half way done with university. In five, done with school. In seven, starting my career as a game developer.\n"; }
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World! Jose Silva. I have goals in the gym, but my main goal is to have a job that allows creativity and pays well. My current interests are the gym, drawing, and gaming.\n"
+              << "My dream is to create a great story that inspires many.\n"
+              << "In one year I expect to be done at MVC. In three, half way done with university. In five, done with school. In seven, starting my career as a game developer.\n";
+    return 0;
+}
